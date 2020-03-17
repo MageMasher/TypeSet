e464094d9b0a58d3d2f24ac76d888d2036a2d095
Joe Lane <me@joelane.net>
2020-03-17 11:13:39 -0500

    use es5 module syntax because Nodejs requires it

--- index.ts
+++ index.ts
@@ -1,2 +1,31 @@
-export * from './src/set'
-export * from './src/core'
+import {
+  difference,
+  index,
+  intersection,
+  join,
+  mapInvert,
+  project,
+  rename,
+  renameKeys,
+  select,
+  union
+} from './src/set'
+import { selectKeys } from './src/core'
+
+
+module.exports = {
+  // Core
+  selectKeys,
+
+  // Set
+  difference,
+  index,
+  intersection,
+  join,
+  mapInvert,
+  project,
+  rename,
+  renameKeys,
+  select,
+  union
+}
